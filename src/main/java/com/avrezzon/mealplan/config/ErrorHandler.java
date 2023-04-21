20d93303652d35831b398fe199d0b5581052615f
Anthony Rezzonico <anthonyrezz@gmail.com>
2023-04-21 14:29:07 -0600

    Carrying over files, setting up mock data

---
+++ src/main/java/com/avrezzon/mealplan/config/ErrorHandler.java
@@ -0,0 +1,15 @@
+package com.avrezzon.mealplan.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalState(IllegalStateException e){
+        return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
